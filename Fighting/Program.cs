21a17e6b0462ca7e71eb3fe84acea2d98cc0410a
Blexander Bolander <alexander.bolander@elev.ga.ntig.se>
2024-09-24 13:07:57 +0200

    yippie

--- Fighting/Program.cs
+++ Fighting/Program.cs
@@ -1,33 +1,51 @@
 ﻿
+int Player1hp = 100;
+int Player2hp = 100;
 
+string Player1name = "player1";
+string Player2name = "player2";
 
+Random generator = new Random();
 
-while (true)
+while (Player1hp > 0 && Player2hp > 0)
 {
-
-    int hp = Random.Shared.Next(100);
-Console.WriteLine(hp);
+Console.WriteLine("\n ____ ==== STARTA NY RUNDA ==== ____");
+Console.WriteLine($"{Player1name}: {Player1hp} {Player2name}: Player2hp\n");
 Console.ReadLine();
 
+int Player1Damage = generator.Next(20);
+Player2hp -= Player1Damage;
+Player2hp = Math.Max(0, Player2hp);
+Console.WriteLine($"{Player1name} gör {Player1Damage} skada på {Player2name}");
+
+int Player2Damage = generator.Next(20);
+Player1hp -= Player2Damage;
+Player1hp = Math.Max(0, Player1hp);
+Console.WriteLine($"{Player2name} gör {Player2Damage} skada på {Player1name}");
+
+Console.WriteLine("Klicka vart du vill på ditt tangentbord för att fortsätta! (inte på avstängningsknappen)");
+Console.ReadKey();
 }
 
+Console.WriteLine("\n ____ ==== Nu är Striden över ==== ____");
 
-
-
-
-Console.WriteLine("Skriv ditt namn");
-string name = Console.ReadLine();
-
- while (name == "")
+if (Player1hp == 0 && Player2hp == 0)
 {
-    Console.WriteLine("Du måste skriva ett namn dumfitta");
-    Console.WriteLine("Försök igen:");
-    name = Console.ReadLine();
+    Console.WriteLine("Ni Båda dog :/");
 
 }
+else if (Player1hp == 0)
+{
+    Console.WriteLine($"{Player2name} Har vunnit, YIPPIE!!! :D");
+}
+else
+{
+    Console.WriteLine($"{Player1name} har vunnit, TJOHOOOOOOOOOOOOOOOOOOOO !!!! ");
+}
+
+Console.WriteLine("tryck på en tangent för att avsluta (inte på avstängningsknappen tack)");
+Console.ReadKey();
 
 
 
-
-Console.ReadLine();
 
